ffc5459a62bb87335743c117f9ff05cb49736176
Eunice Choi <eunicechoistudio@gmail.com>
2018-08-26 21:02:49 -0400

    updated api controller testings

--- spec/controllers/api/v1/check_ins_controller_spec.rb
+++ spec/controllers/api/v1/check_ins_controller_spec.rb
@@ -9,13 +9,15 @@ RSpec.describe Api::V1::CheckInsController, type: :controller do
     it "should return a list of all the check ins" do
       sign_in user
 
-      get :index, params: {habit_id: 1}
+      get :index, params: {habit_id: habit}
       returned_json = JSON.parse(response.body)
 
       expect(response.status).to eq 200
       expect(response.content_type).to eq("application/json")
       expect(returned_json.length).to eq 1
+      expect(returned_json["check_ins"].length).to eq 21
       expect(returned_json["check_ins"][0]["complete"]).to eq false
+      expect(returned_json["check_ins"][1]["complete"]).to eq false
     end
   end
 end
